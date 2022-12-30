d3a9c0a2d9a33641c46046a8c13a55f47d2dced5
matt530bailey <115194527+matt530bailey@users.noreply.github.com>
2022-12-30 12:45:38 -0800

    created fetch requests for 5day and current weather forcasts

--- script.js
+++ script.js
@@ -2,9 +2,45 @@ const weatherHome = document.querySelector('#weatherStation');
 const locationReq = document.querySelector('#location');
 const searchBtn = document.querySelector('#selectBtn')
 
-function reqApi() {
-    const weatherApi = 'api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={49df966d9c253f90278d84a948be3d6e}';
-    fetch(weatherApi)
+
+function fiveDayForcast(cityName) {
+    console.log(cityName)
+        fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&units=imperial&appid=49df966d9c253f90278d84a948be3d6e`).then(response => {
+            return response.json()
+        }).then(data => {
+            console.log(data)
+        })
+        fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=imperial&appid=49df966d9c253f90278d84a948be3d6e`).then(response => {
+            return response.json()
+        }).then(data => { 
+            var currentWeatherCard = document.createElement('div')
+            var cityDate = document.createElement('h1')
+            var currentTemp = document.createElement('h3')
+            var currentWind = document.createElement('h3')
+            var currentHumid = document.createElement('h3')
+
+            cityDate.innerHTML = data.name + (new Date().toLocaleDateString());
+            currentWeatherCard.appendChild(cityDate);
+            weatherHome.appendChild(currentWeatherCard);
+
+            console.log(data)
+        })
+            
+}
+
+function cityToGeo(location) {
+    const cityUrlApi = `http://api.openweathermap.org/geo/1.0/direct?q=${encodeURIComponent(location)}&limit={limit}&appid={49df966d9c253f90278d84a948be3d6e}`;
+    // TODO: Write a fetch to transofrm city/state/zip "location" string into a lat-long
+    fetch(cityUrlApi)
+        .then(function (response) {
+            return response.json();
+        })
+    // TODO: Supply lat -log to your `reqApi` function
+}
+
+function reqApi(lat, lon) {
+    const latLonApi = 'api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={49df966d9c253f90278d84a948be3d6e}';
+    fetch(latLonApi)
         .then(function (response) {
             return response.json();
         }).then(data => {
@@ -13,15 +49,19 @@ function reqApi() {
             weatherHome.appendChild(dashBoard);
             console.log(data);
         }).catch(err => {
-            console.log(err);})
-            
-    }
-
-    function inputField() {
-        searchBtn.addEventListener('click', () => {
-            localStorage.setItem('city', locationReq.value);
-            console.log(localStorage.getItem('city'));
+            console.log(err);
         })
-    }
 
+}
+
+function inputField() {
+    searchBtn.addEventListener('click', () => {
+        localStorage.setItem('city', locationReq.value);
+        //console.log(localStorage.getItem('city'));
+        // cityToGeo(locationReq.value);
+        fiveDayForcast(locationReq.value);
+    })
+}
+
+inputField();
 
